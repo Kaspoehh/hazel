32c0186f23afe2bae5196ea0324443f53e48d21c
kaspoehh <kasperbruin8@gmail.com>
2021-05-26 18:34:28 +0200

    Lua

--- Hazel/src/Hazel/Log.cpp
+++ Hazel/src/Hazel/Log.cpp
@@ -10,6 +10,7 @@ namespace Hazel
     void Log::Init()
     {
         spdlog::set_pattern("%^[%T] %n: %v%$");
+        
         s_CoreLogger = spdlog::stdout_color_mt("HAZEL");
         s_CoreLogger->set_level(spdlog::level::trace);
 
